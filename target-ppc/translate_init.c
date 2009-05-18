98f29b341a6e5f317c322bfe37a631a3827ccae2
gingold <gingold@020d506d-db78-4e55-b2a8-6c851f84c332>
2009-05-18 10:18:34 +0000

    init_excp_7x0: fix hreset entry point. Part of I514-015.

--- target-ppc/translate_init.c
+++ target-ppc/translate_init.c
@@ -2925,7 +2925,7 @@ static void init_excp_7x0 (CPUPPCState *env)
     env->excp_vectors[POWERPC_EXCP_THERM]    = 0x00001700;
     env->hreset_excp_prefix = 0x00000000UL;
     /* Hardware reset vector */
-    env->hreset_vector = 0xFFFFFFFCUL;
+    env->hreset_vector = 0xFFF00100UL;
 #endif
 }
 
