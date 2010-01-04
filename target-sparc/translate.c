996c30cd266536b29803e495720a2476860326ae
Tristan Gingold <gingold@chinon.act-europe.fr>
2010-01-04 14:43:51 +0100

    Add an abort on code that should never be executed.

--- target-sparc/translate.c
+++ target-sparc/translate.c
@@ -1013,6 +1013,7 @@ static inline void flush_cond(DisasContext *dc)
     if (dc->npc == JUMP_PC) {
         gen_generic_branch(dc);
         dc->npc = DYNAMIC_PC;
+	abort(); /* TG: shouldn't happen.  */
     }
 }
 
