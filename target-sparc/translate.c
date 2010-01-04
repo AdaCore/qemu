15379b851ab5d6a16323912dc9e41c9f80250f14
Tristan Gingold <gingold@chinon.act-europe.fr>
2010-01-04 14:44:19 +0100

    sparc: return branch taken on tb exit.
    --- merged with ---
    
    target_sparc: Fix typo and use TB_EXIT macros
    
    Part of NA24-010.

--- target-sparc/translate.c
+++ target-sparc/translate.c
@@ -330,7 +330,7 @@ static inline void gen_goto_tb(DisasContext *s, int tb_num,
         /* jump to another page: currently not optimized */
         tcg_gen_movi_tl(cpu_pc, pc);
         tcg_gen_movi_tl(cpu_npc, npc);
-        tcg_gen_exit_tb(0);
+        tcg_gen_exit_tb(((uintptr_t)tb + tb_num) | TB_EXIT_NOPATCH);
     }
 }
 
