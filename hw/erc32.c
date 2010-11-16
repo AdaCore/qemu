52658fd4c58e272e6b73b893d7df4662935a19d0
Tristan Gingold <gingold@adacore.com>
2010-11-16 15:49:44 +0100

    Fix header.

--- hw/erc32.c
+++ hw/erc32.c
@@ -1,7 +1,7 @@
 /*
  * QEMU Erc32 System Emulator
  *
-   Copyright (C) 2010, AdaCore
+ * Copyright (C) 2010 AdaCore
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
