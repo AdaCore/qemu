345b892f34ba683df6220cc315b9005c661e58aa
Fabien Chouteau <chouteau@adacore.com>
2011-01-24 14:57:32 +0100

    IPv4 as default address family for TCP connections

--- qemu-sockets.c
+++ qemu-sockets.c
@@ -114,7 +114,7 @@ int inet_listen_opts(QemuOpts *opts, int port_offset, Error **errp)
 
     memset(&ai,0, sizeof(ai));
     ai.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
-    ai.ai_family = PF_UNSPEC;
+    ai.ai_family = PF_INET;     /* IPv4 is the default address family */
     ai.ai_socktype = SOCK_STREAM;
 
     if ((qemu_opt_get(opts, "host") == NULL) ||
@@ -218,7 +218,7 @@ int inet_connect_opts(QemuOpts *opts, Error **errp)
 
     memset(&ai,0, sizeof(ai));
     ai.ai_flags = AI_CANONNAME | AI_ADDRCONFIG;
-    ai.ai_family = PF_UNSPEC;
+    ai.ai_family = PF_INET;     /* IPv4 is the default address family */
     ai.ai_socktype = SOCK_STREAM;
 
     addr = qemu_opt_get(opts, "host");
@@ -304,7 +304,7 @@ int inet_dgram_opts(QemuOpts *opts)
     /* lookup peer addr */
     memset(&ai,0, sizeof(ai));
     ai.ai_flags = AI_CANONNAME | AI_ADDRCONFIG;
-    ai.ai_family = PF_UNSPEC;
+    ai.ai_family = PF_INET;     /* IPv4 is the default address family */
     ai.ai_socktype = SOCK_DGRAM;
 
     addr = qemu_opt_get(opts, "host");
