b82469c906829865693b4fad0edb0a069604a8d9
Fabien Chouteau <chouteau@adacore.com>
2011-01-28 11:42:35 +0100

    Add traces generation
    Part of I108-020.

--- vl.c
+++ vl.c
@@ -165,6 +165,8 @@ int main(int argc, char **argv)
 
 #include "ui/qemu-spice.h"
 
+#include "qemu-traces.h"
+
 //#define DEBUG_NET
 //#define DEBUG_SLIRP
 
@@ -3152,6 +3154,10 @@ int main(int argc, char **argv, char **envp)
                     fclose(fp);
                     break;
                 }
+            case QEMU_OPTION_exec_trace:
+                trace_init(optarg);
+                break;
+
             default:
                 os_parse_cmd_args(popt->index, optarg);
             }
