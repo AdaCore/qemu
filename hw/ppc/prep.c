51bcc6721c8d1f04a67b4822a9a8c1a96bfaad85
Tristan Gingold <gingold@adacore.com>
2011-02-04 14:14:51 +0100

    Allow to boot without a boot device.

--- hw/ppc/prep.c
+++ hw/ppc/prep.c
@@ -603,10 +603,6 @@ static void ppc_prep_init(MachineState *machine)
                 break;
             }
         }
-        if (ppc_boot_device == '\0') {
-            fprintf(stderr, "No valid boot device for Mac99 machine\n");
-            exit(1);
-        }
     }
 
     if (PPC_INPUT(env) != PPC_FLAGS_INPUT_6xx) {
