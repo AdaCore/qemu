5c8efdcf57ba3d8b614019cf49b496c450a074b1
Fabien Chouteau <chouteau@adacore.com>
2011-02-07 16:19:44 +0100

    Fix Copyright notice

--- hw/grlib.h
+++ hw/grlib.h
@@ -1,7 +1,7 @@
 /*
  * QEMU GRLIB Components
  *
- * Copyright (c) 2010-2011 AdaCore
+ * Copyright (c) 2010-2011, AdaCore
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
