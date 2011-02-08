25d7fbe7267566f32d5a70f38de937ee1b581fa2
Tristan Gingold <gingold@adacore.com>
2011-02-08 11:04:25 +0100

    ppc_prep: allow more than one serial port.
    Signed-off-by: Tristan Gingold <gingold@adacore.com>

--- hw/ppc_prep.c
+++ hw/ppc_prep.c
@@ -667,8 +667,12 @@ static void ppc_prep_init (ram_addr_t ram_size,
     //    pit = pit_init(0x40, i8259[0]);
     rtc_init(2000, NULL);
 
-    if (serial_hds[0])
-        serial_isa_init(0, serial_hds[0]);
+    for(i = 0; i < MAX_SERIAL_PORTS; i++) {
+        if (serial_hds[i]) {
+            serial_isa_init(i, serial_hds[i]);
+        }
+    }
+
     nb_nics1 = nb_nics;
     if (nb_nics1 > NE2000_NB_MAX)
         nb_nics1 = NE2000_NB_MAX;
