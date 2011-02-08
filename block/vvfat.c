985c0b551ab54064c2c876c47f05bb567688ebe1
Tristan Gingold <gingold@adacore.com>
2011-02-08 13:54:53 +0100

    vvfat: introduce a no-mbr option and handle last block data
    This improves the vvfat support in ordre to cleanup the temporary files and
    add a no-mbr option.
    
    2018-07-25  KONRAD Frederic  <konrad@adacore.com>
            * block/vvfat.c: fixup no-mbr.
    
    Part of L329-018, U706-019
    Change-Id: I0c82ddf1e633b9f951f45f069768b14d536e1715

--- block/vvfat.c
+++ block/vvfat.c
@@ -269,6 +269,7 @@ typedef struct mapping_t {
          */
         struct {
             uint32_t offset;
+            char *last_cluster_data;
         } file;
         struct {
             int parent_mapping_index;
@@ -281,9 +282,8 @@ typedef struct mapping_t {
     enum {
         MODE_UNDEFINED = 0,
         MODE_NORMAL = 1,
-        MODE_MODIFIED = 2,
-        MODE_DIRECTORY = 4,
-        MODE_DELETED = 8,
+        MODE_DIRECTORY = 2,
+        MODE_DELETED = 4,
     } mode;
     int read_only;
 } mapping_t;
@@ -843,6 +843,7 @@ static int read_directory(BDRVVVFATState* s, int mapping_index)
             } else {
                 s->current_mapping->mode = MODE_UNDEFINED;
                 s->current_mapping->info.file.offset = 0;
+                s->current_mapping->info.file.last_cluster_data = NULL;
             }
             s->current_mapping->path=buffer;
             s->current_mapping->read_only =
@@ -884,7 +885,11 @@ static int read_directory(BDRVVVFATState* s, int mapping_index)
 
 static inline int32_t sector2cluster(BDRVVVFATState* s,off_t sector_num)
 {
-    return (sector_num - s->offset_to_root_dir) / s->sectors_per_cluster;
+    if (sector_num < s->offset_to_root_dir) {
+        return -1;
+    } else {
+        return (sector_num-s->offset_to_root_dir) / s->sectors_per_cluster;
+    }
 }
 
 static inline off_t cluster2sector(BDRVVVFATState* s, uint32_t cluster_num)
@@ -917,6 +922,7 @@ static int init_directories(BDRVVVFATState* s,
     s->sectors_per_fat=(s->sector_count+i)/i; /* round up */
 
     s->offset_to_fat = s->offset_to_bootsector + 1;
+
     s->offset_to_root_dir = s->offset_to_fat + s->sectors_per_fat * 2;
 
     array_init(&(s->mapping),sizeof(mapping_t));
@@ -1082,6 +1088,11 @@ static QemuOptsList runtime_opts = {
             .type = QEMU_OPT_BOOL,
             .help = "Make the image writable",
         },
+        {
+            .name = "no-mbr",
+            .type = QEMU_OPT_BOOL,
+            .help = "Do not add MBR on this disk",
+        },
         { /* end of list */ }
     },
 };
@@ -1092,6 +1103,7 @@ static void vvfat_parse_filename(const char *filename, QDict *options,
     int fat_type = 0;
     bool floppy = false;
     bool rw = false;
+    bool no_mbr = false;
     int i;
 
     if (!strstart(filename, "fat:", NULL)) {
@@ -1116,6 +1128,10 @@ static void vvfat_parse_filename(const char *filename, QDict *options,
         rw = true;
     }
 
+    if (strstr(filename, ":no-mbr:")) {
+        no_mbr = true;
+    }
+
     /* Get the directory name without options */
     i = strrchr(filename, ':') - filename;
     assert(i >= 3);
@@ -1131,6 +1147,7 @@ static void vvfat_parse_filename(const char *filename, QDict *options,
     qdict_put_int(options, "fat-type", fat_type);
     qdict_put_bool(options, "floppy", floppy);
     qdict_put_bool(options, "rw", rw);
+    qdict_put_bool(options, "no-mbr", no_mbr);
 }
 
 static int vvfat_open(BlockDriverState *bs, QDict *options, int flags,
@@ -1194,7 +1211,10 @@ static int vvfat_open(BlockDriverState *bs, QDict *options, int flags,
         if (!s->fat_type) {
             s->fat_type = 16;
         }
-        s->offset_to_bootsector = 0x3f;
+        if (!qemu_opt_get_bool(opts, "no-mbr", false)) {
+            /* We have an MBR in this case, it stay zero otherwise.. */
+            s->offset_to_bootsector = 0x3f;
+        }
         cyls = s->fat_type == 12 ? 64 : 1024;
         heads = 16;
         secs = 63;
@@ -1415,6 +1435,14 @@ read_cluster_directory:
             s->current_cluster = -1;
             return -1;
         }
+        if (cluster_num == s->current_mapping->end - 1
+            && s->current_mapping->info.file.last_cluster_data) {
+            memcpy (s->cluster + result,
+                    s->current_mapping->info.file.last_cluster_data + result,
+                    s->cluster_size - result);
+        } else {
+            memset (s->cluster + result, 0, s->cluster_size - result);
+        }
         s->current_cluster = cluster_num;
     }
     return 0;
@@ -1593,6 +1621,11 @@ typedef struct commit_t {
         ACTION_RENAME, ACTION_WRITEOUT, ACTION_NEW_FILE, ACTION_MKDIR
     } action;
 } commit_t;
+
+#ifdef DEBUG
+static const char * const action_str[] =
+  { "rename", "writeout", "new_file", "mkdir" };
+#endif
 
 static void clear_commits(BDRVVVFATState* s)
 {
@@ -1872,6 +1905,7 @@ static uint32_t get_cluster_count_for_direntry(BDRVVVFATState* s,
      */
     int copy_it = 0;
     int was_modified = 0;
+    int size_was_modified = 0;
     int32_t ret = 0;
 
     uint32_t cluster_num = begin_of_direntry(direntry);
@@ -1894,6 +1928,7 @@ static uint32_t get_cluster_count_for_direntry(BDRVVVFATState* s,
 
         if (mapping) {
             const char* basename;
+            direntry_t *d;
 
             assert(mapping->mode & MODE_DELETED);
             mapping->mode &= ~MODE_DELETED;
@@ -1903,8 +1938,15 @@ static uint32_t get_cluster_count_for_direntry(BDRVVVFATState* s,
             assert(mapping->mode & MODE_NORMAL);
 
             /* rename */
-            if (strcmp(basename, basename2))
+            if (strcmp(basename, basename2)) {
                 schedule_rename(s, cluster_num, g_strdup(path));
+            }
+
+            d = (direntry_t*)array_get(&(s->directory), mapping->dir_index);
+            if ((filesize_of_direntry(d) & 0x1ff)
+                != (filesize_of_direntry(direntry) & 0x1ff)) {
+                size_was_modified = 1;
+            }
         } else if (is_file(direntry))
             /* new file */
             schedule_new_file(s, g_strdup(path), cluster_num);
@@ -1994,10 +2036,15 @@ static uint32_t get_cluster_count_for_direntry(BDRVVVFATState* s,
 
         cluster_num = modified_fat_get(s, cluster_num);
 
-        if (fat_eof(s, cluster_num))
+        if (fat_eof(s, cluster_num)) {
+            if (size_was_modified && !was_modified) {
+                schedule_writeout(s, mapping->dir_index,
+                                  filesize_of_direntry(direntry) & ~0x1ff);
+            }
             return ret;
-        else if (cluster_num < 2 || cluster_num > s->max_fat_value - 16)
+        } else if (cluster_num < 2 || cluster_num > s->max_fat_value - 16) {
             return -1;
+        }
 
         offset += s->cluster_size;
     }
@@ -2275,8 +2322,12 @@ static int remove_mapping(BDRVVVFATState* s, int mapping_index)
     mapping_t* first_mapping = array_get(&(s->mapping), 0);
 
     /* free mapping */
-    if (mapping->first_mapping_index < 0) {
-        g_free(mapping->path);
+    if (mapping->first_mapping_index < 0)
+        free(mapping->path);
+    if ((mapping->mode & MODE_NORMAL)
+        && mapping->info.file.last_cluster_data) {
+        free (mapping->info.file.last_cluster_data);
+        mapping->info.file.last_cluster_data = NULL;
     }
 
     /* remove from s->mapping */
@@ -2398,10 +2449,11 @@ static int commit_mappings(BDRVVVFATState* s,
                         mapping->info.dir.first_dir_index +
                         0x10 * s->sectors_per_cluster *
                         (mapping->end - mapping->begin);
-            } else
+            } else {
                 next_mapping->info.file.offset = mapping->info.file.offset +
                         mapping->end - mapping->begin;
-
+                next_mapping->info.file.last_cluster_data = NULL;
+            }
             mapping = next_mapping;
         }
 
@@ -2462,16 +2514,11 @@ static int commit_direntries(BDRVVVFATState* s,
                 factor * (old_cluster_count - new_cluster_count));
 
     for (c = first_cluster; !fat_eof(s, c); c = modified_fat_get(s, c)) {
-        direntry_t *first_direntry;
         void* direntry = array_get(&(s->directory), current_dir_index);
         int ret = vvfat_read(s->bs, cluster2sector(s, c), direntry,
                 s->sectors_per_cluster);
         if (ret)
             return ret;
-
-        /* The first directory entry on the filesystem is the volume name */
-        first_direntry = (direntry_t*) s->directory.pointer;
-        assert(!memcmp(first_direntry->name, s->volume_label, 11));
 
         current_dir_index += factor;
     }
@@ -2520,6 +2567,9 @@ static int commit_one_file(BDRVVVFATState* s,
         return -1;
     }
 
+    DLOG(fprintf(stderr,"commit_one_file: %s at %d (len=%d)\n",
+                 mapping->path, offset, size));
+
     for (i = s->cluster_size; i < offset; i += s->cluster_size)
         c = modified_fat_get(s, c);
 
@@ -2550,7 +2600,7 @@ static int commit_one_file(BDRVVVFATState* s,
                 (size > offset && c >=2 && !fat_eof(s, c)));
 
         ret = vvfat_read(s->bs, cluster2sector(s, c),
-            (uint8_t*)cluster, DIV_ROUND_UP(rest_size, 0x200));
+                         (uint8_t*)cluster, s->cluster_size / 0x200);
 
         if (ret < 0) {
             qemu_close(fd);
@@ -2562,6 +2612,15 @@ static int commit_one_file(BDRVVVFATState* s,
             qemu_close(fd);
             g_free(cluster);
             return -2;
+        }
+
+        if (rest_size != s->cluster_size) {
+            mapping_t *mapping = find_mapping_for_cluster(s, c);
+            assert (mapping);
+            if (!mapping->info.file.last_cluster_data)
+                mapping->info.file.last_cluster_data=malloc (s->cluster_size);
+            memcpy (mapping->info.file.last_cluster_data, cluster,
+                    s->cluster_size);
         }
 
         offset += rest_size;
@@ -2588,7 +2647,7 @@ static void check1(BDRVVVFATState* s)
     for (i = 0; i < s->mapping.next; i++) {
         mapping_t* mapping = array_get(&(s->mapping), i);
         if (mapping->mode & MODE_DELETED) {
-            fprintf(stderr, "deleted\n");
+            fprintf(stderr, "deleted: %s\n", mapping->path);
             continue;
         }
         assert(mapping->dir_index < s->directory.next);
@@ -2657,9 +2716,9 @@ static int handle_renames_and_mkdirs(BDRVVVFATState* s)
     fprintf(stderr, "handle_renames\n");
     for (i = 0; i < s->commits.next; i++) {
         commit_t* commit = array_get(&(s->commits), i);
-        fprintf(stderr, "%d, %s (%u, %d)\n", i,
+        fprintf(stderr, "%d, %s (%u, %s)\n", i,
                 commit->path ? commit->path : "(null)",
-                commit->param.rename.cluster, commit->action);
+                commit->param.rename.cluster, action_str[commit->action]);
     }
 #endif
 
@@ -2835,6 +2894,7 @@ static int handle_commits(BDRVVVFATState* s)
             mapping->read_only = 0;
             mapping->mode = MODE_NORMAL;
             mapping->info.file.offset = 0;
+            mapping->info.file.last_cluster_data = NULL;
 
             if (commit_one_file(s, i, 0))
                 fail = -7;
@@ -2863,21 +2923,20 @@ static int handle_deletes(BDRVVVFATState* s)
         for (i = 1; i < s->mapping.next; i++) {
             mapping_t* mapping = array_get(&(s->mapping), i);
             if (mapping->mode & MODE_DELETED) {
-                direntry_t* entry = array_get(&(s->directory),
-                        mapping->dir_index);
-
-                if (is_free(entry)) {
-                    /* remove file/directory */
-                    if (mapping->mode & MODE_DIRECTORY) {
-                        int j, next_dir_index = s->directory.next,
+                DLOG(direntry_t* entry = array_get(&(s->directory),
+                                                   mapping->dir_index));
+                /* remove file/directory */
+                if (mapping->mode & MODE_DIRECTORY) {
+                    int j, next_dir_index = s->directory.next,
                         first_dir_index = mapping->info.dir.first_dir_index;
 
                         if (rmdir(mapping->path) < 0) {
                             if (errno == ENOTEMPTY) {
                                 deferred++;
                                 continue;
-                            } else
+                            } else {
                                 return -5;
+                            }
                         }
 
                         for (j = 1; j < s->mapping.next; j++) {
@@ -2894,7 +2953,6 @@ static int handle_deletes(BDRVVVFATState* s)
                                 next_dir_index - first_dir_index);
 
                         deleted++;
-                    }
                 } else {
                     if (unlink(mapping->path))
                         return -4;
@@ -2920,10 +2978,6 @@ static int handle_deletes(BDRVVVFATState* s)
 static int do_commit(BDRVVVFATState* s)
 {
     int ret = 0;
-
-    /* the real meat are the commits. Nothing to do? Move along! */
-    if (s->commits.next == 0)
-        return 0;
 
     vvfat_close_current_file(s);
 
@@ -2971,8 +3025,10 @@ static int try_commit(BDRVVVFATState* s)
 {
     vvfat_close_current_file(s);
 DLOG(checkpoint());
-    if(!is_consistent(s))
+    if(!is_consistent(s)) {
+        DLOG(fprintf(stderr,"not consistent\n"));
         return -1;
+    }
     return do_commit(s);
 }
 
@@ -3207,8 +3263,15 @@ static void vvfat_child_perm(BlockDriverState *bs, BdrvChild *c,
 static void vvfat_close(BlockDriverState *bs)
 {
     BDRVVVFATState *s = bs->opaque;
+    int i;
 
     vvfat_close_current_file(s);
+
+    for (i = 0; i < s->mapping.next; i++) {
+	mapping_t *mapping=array_get(&(s->mapping),i);
+	if (mapping->first_mapping_index < 0)
+	    free (mapping->path);
+    }
     array_free(&(s->fat));
     array_free(&(s->directory));
     array_free(&(s->mapping));
