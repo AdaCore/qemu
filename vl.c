491a1de7d22929ff81c5f2c565d6862aadcb8821
Fabien Chouteau <chouteau@adacore.com>
2011-02-18 16:03:16 +0100

    First version of the Qemu Plug-in interface
     - Device registration
     - I/O memory registration
     - I/O read/write
     - DMA read/write
     - IRQ
     - Events
    
    An example is provided in directory plugin/sample/
    
    Part of K125-030.

--- vl.c
+++ vl.c
@@ -164,6 +164,7 @@ int main(int argc, char **argv)
 #include "ui/qemu-spice.h"
 
 #include "qemu-traces.h"
+#include "qemu-plugin.h"
 
 //#define DEBUG_NET
 //#define DEBUG_SLIRP
@@ -2880,6 +2881,10 @@ int main(int argc, char **argv, char **envp)
                 }
             case QEMU_OPTION_exec_trace:
                 trace_init(optarg);
+                break;
+
+            case QEMU_OPTION_plugin:
+                plugin_save_optargs(optarg);
                 break;
 
             default:
