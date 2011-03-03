0001fb8dc1621e86168948ce3d1c7d98a09a6df3
Tristan Gingold <gingold@adacore.com>
2011-03-03 17:39:10 +0100

    powerpc: handle gdb single step for blr.
    FIXME: also handle cpu single-step ?
    Fix for gdb-testsuite G130-022__next_from_last_fun_line.

--- target-ppc/translate.c
+++ target-ppc/translate.c
@@ -3571,6 +3571,9 @@ static inline void gen_bcond(DisasContext *ctx, int type)
         else
 #endif
             tcg_gen_andi_tl(cpu_nip, target, ~3);
+        if (ctx->singlestep_enabled & GDBSTUB_SINGLE_STEP) {
+            gen_debug_exception(ctx);
+        }
         tcg_gen_exit_tb((long)ctx->tb + 4);
     }
 
