891ef4160b4397083c01aea96b89eba0b35c9780
Tristan Gingold <gingold@adacore.com>
2011-03-07 14:46:32 +0100

    powerpc: slightly optimize bclr/bcctr and helps couverture.
    Part of I519-015.

--- target-ppc/translate.c
+++ target-ppc/translate.c
@@ -3883,6 +3883,7 @@ static inline void gen_bcond(DisasContext *ctx, int type)
     TCGv target;
 
     ctx->exception = POWERPC_EXCP_BRANCH;
+    /* Read target for branch to register.  */
     if (type == BCOND_LR || type == BCOND_CTR || type == BCOND_TAR) {
         target = tcg_temp_local_new();
         if (type == BCOND_CTR)
@@ -3894,12 +3895,21 @@ static inline void gen_bcond(DisasContext *ctx, int type)
     } else {
         TCGV_UNUSED(target);
     }
-    if (LK(ctx->opcode))
+
+    /* Save LR for link variants.  */
+    if (LK(ctx->opcode)) {
         gen_setlr(ctx, ctx->nip);
-    l1 = gen_new_label();
+    }
+
+    /* In some variants (CTR test or CR test), the branch is conditionnal.  */
+    if ((bo & 0x14) != 0) {
+        l1 = gen_new_label();
+    }
+
     if ((bo & 0x4) == 0) {
         /* Decrement and test CTR */
         TCGv temp = tcg_temp_new();
+
         if (unlikely(type == BCOND_CTR)) {
             gen_inval_exception(ctx, POWERPC_EXCP_INVAL_INVAL);
             return;
@@ -3910,11 +3920,8 @@ static inline void gen_bcond(DisasContext *ctx, int type)
         } else {
             tcg_gen_mov_tl(temp, cpu_ctr);
         }
-        if (bo & 0x2) {
-            tcg_gen_brcondi_tl(TCG_COND_NE, temp, 0, l1);
-        } else {
-            tcg_gen_brcondi_tl(TCG_COND_EQ, temp, 0, l1);
-        }
+        tcg_gen_brcondi_tl((bo & 0x2) ? TCG_COND_NE : TCG_COND_EQ,
+                           temp, 0, l1);
         tcg_temp_free(temp);
     }
     if ((bo & 0x10) == 0) {
@@ -3923,25 +3930,22 @@ static inline void gen_bcond(DisasContext *ctx, int type)
         uint32_t mask = 0x08 >> (bi & 0x03);
         TCGv_i32 temp = tcg_temp_new_i32();
 
-        if (bo & 0x8) {
-            tcg_gen_andi_i32(temp, cpu_crf[bi >> 2], mask);
-            tcg_gen_brcondi_i32(TCG_COND_EQ, temp, 0, l1);
-        } else {
-            tcg_gen_andi_i32(temp, cpu_crf[bi >> 2], mask);
-            tcg_gen_brcondi_i32(TCG_COND_NE, temp, 0, l1);
-        }
+        tcg_gen_andi_i32(temp, cpu_crf[bi >> 2], mask);
+        tcg_gen_brcondi_i32((bo & 0x8) ? TCG_COND_EQ : TCG_COND_NE,
+                            temp, 0, l1);
         tcg_temp_free_i32(temp);
     }
     gen_update_cfar(ctx, ctx->nip);
     if (type == BCOND_IM) {
         target_ulong li = (target_long)((int16_t)(BD(ctx->opcode)));
+        target_ulong dst;
+
         if (likely(AA(ctx->opcode) == 0)) {
-            gen_goto_tb(ctx, 0, ctx->nip + li - 4);
+            dst = ctx->nip + li - 4;
         } else {
-            gen_goto_tb(ctx, 0, li);
+            dst = li;
         }
-        gen_set_label(l1);
-        gen_goto_tb(ctx, 1, ctx->nip);
+        gen_goto_tb(ctx, 0, dst);
     } else {
         if (NARROW_MODE(ctx)) {
             tcg_gen_andi_tl(cpu_nip, target, (uint32_t)~3);
@@ -3949,9 +3953,11 @@ static inline void gen_bcond(DisasContext *ctx, int type)
             tcg_gen_andi_tl(cpu_nip, target, ~3);
         }
         tcg_gen_exit_tb((long)ctx->tb + TB_EXIT_NOPATCH);
+    }
+
+    if ((bo & 0x14) != 0) {
         gen_set_label(l1);
-        gen_update_nip(ctx, ctx->nip);
-        tcg_gen_exit_tb(0);
+        gen_goto_tb(ctx, 1, ctx->nip);
     }
     if (type == BCOND_LR || type == BCOND_CTR || type == BCOND_TAR) {
         tcg_temp_free(target);
