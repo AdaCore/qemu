315c457f799d85434e2bac88951a6af84281d8ad
Tristan Gingold <gingold@adacore.com>
2011-03-23 15:06:55 +0100

    powerpc: correctly handle fpu exceptions.
    Raise the exception on the first occurence, do not wait for the next
    floating point operation.

--- target-ppc/op_helper.c
+++ target-ppc/op_helper.c
@@ -976,22 +976,22 @@ void helper_store_fpscr (uint64_t arg, uint32_t mask)
 
 void helper_float_check_status (void)
 {
+    int status = get_float_exception_flags(&env->fp_status);
+    if (status & float_flag_divbyzero) {
+        float_zero_divide_excp();
+    } else if (status & float_flag_overflow) {
+        float_overflow_excp();
+    } else if (status & float_flag_underflow) {
+        float_underflow_excp();
+    } else if (status & float_flag_inexact) {
+        float_inexact_excp();
+    }
+
     if (env->exception_index == POWERPC_EXCP_PROGRAM &&
         (env->error_code & POWERPC_EXCP_FP)) {
         /* Differred floating-point exception after target FPR update */
         if (msr_fe0 != 0 || msr_fe1 != 0)
             helper_raise_exception_err(env->exception_index, env->error_code);
-    } else {
-        int status = get_float_exception_flags(&env->fp_status);
-        if (status & float_flag_divbyzero) {
-            float_zero_divide_excp();
-        } else if (status & float_flag_overflow) {
-            float_overflow_excp();
-        } else if (status & float_flag_underflow) {
-            float_underflow_excp();
-        } else if (status & float_flag_inexact) {
-            float_inexact_excp();
-        }
     }
 }
 
