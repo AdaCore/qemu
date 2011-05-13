8c1f6147c0cbc6daa8188d9a2d3fee4c4d1c9097
Fabien Chouteau <chouteau@adacore.com>
2011-05-13 15:24:56 +0200

    booke timers
    While working on the emulation of the freescale p2010 (e500v2) I realized that
    there's no implementation of booke's timers features. Currently mpc8544 uses
    ppc_emb (ppc_emb_timers_init) which is close but not exactly like booke (for
    example booke uses different SPR).
    
    Part of JB15-027.

--- target-ppc/helper.c
+++ target-ppc/helper.c
@@ -2974,7 +2974,9 @@ void ppc_hw_interrupt (CPUPPCState *env)
     if (msr_ee != 0) {
         /* Watchdog timer on embedded PowerPC */
         if (env->pending_interrupts & (1 << PPC_INTERRUPT_WDT)) {
-            env->pending_interrupts &= ~(1 << PPC_INTERRUPT_WDT);
+            if (env->excp_model != POWERPC_EXCP_BOOKE) {
+                env->pending_interrupts &= ~(1 << PPC_INTERRUPT_WDT);
+            }
             powerpc_excp(env, env->excp_model, POWERPC_EXCP_WDT);
             return;
         }
@@ -2985,7 +2987,9 @@ void ppc_hw_interrupt (CPUPPCState *env)
         }
         /* Fixed interval timer on embedded PowerPC */
         if (env->pending_interrupts & (1 << PPC_INTERRUPT_FIT)) {
-            env->pending_interrupts &= ~(1 << PPC_INTERRUPT_FIT);
+            if (env->excp_model != POWERPC_EXCP_BOOKE) {
+                env->pending_interrupts &= ~(1 << PPC_INTERRUPT_FIT);
+            }
             powerpc_excp(env, env->excp_model, POWERPC_EXCP_FIT);
             return;
         }
@@ -2997,7 +3001,9 @@ void ppc_hw_interrupt (CPUPPCState *env)
         }
         /* Decrementer exception */
         if (env->pending_interrupts & (1 << PPC_INTERRUPT_DECR)) {
-            env->pending_interrupts &= ~(1 << PPC_INTERRUPT_DECR);
+            if (env->excp_model != POWERPC_EXCP_BOOKE) {
+                env->pending_interrupts &= ~(1 << PPC_INTERRUPT_DECR);
+            }
             powerpc_excp(env, env->excp_model, POWERPC_EXCP_DECR);
             return;
         }
