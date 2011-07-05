2cb392c03a6bdbfc91a7e2bc3fdc9d5addc9e2fb
Fabien Chouteau <chouteau@adacore.com>
2011-07-05 14:24:46 +0200

    Fix exec-trace generation on linux-user mode (cleanup)
    Call trace_cleanup at exit.
    
    TBM: Add -exec-trace for linux user processes
    
    Part of K705-003.

--- linux-user/syscall.c
+++ linux-user/syscall.c
@@ -4408,6 +4408,7 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
         _mcleanup();
 #endif
         gdb_exit(cpu_env, arg1);
+        trace_cleanup();
         _exit(arg1);
         ret = 0; /* avoid warning */
         break;
@@ -6033,6 +6034,7 @@ abi_long do_syscall(void *cpu_env, int num, abi_long arg1,
         _mcleanup();
 #endif
         gdb_exit(cpu_env, arg1);
+        trace_cleanup();
         ret = get_errno(exit_group(arg1));
         break;
 #endif
