4299a581e8a1cc2d7df16cf9134e92fa4342bca0
Fabien Chouteau <chouteau@adacore.com>
2011-07-07 11:54:33 +0200

    Fix compiling without MREMAP_FIXED
    Part of K705-003.

--- linux-user/mmap.c
+++ linux-user/mmap.c
@@ -676,6 +676,7 @@ abi_long target_mremap(abi_ulong old_addr, abi_ulong old_size,
 
     mmap_lock();
 
+#if defined (MREMAP_FIXED)
     if (flags & MREMAP_FIXED) {
         host_addr = (void *) syscall(__NR_mremap, g2h(old_addr),
                                      old_size, new_size,
@@ -704,7 +705,9 @@ abi_long target_mremap(abi_ulong old_addr, abi_ulong old_size,
                 mmap_reserve(old_addr, old_size);
             }
         }
-    } else {
+    } else
+#endif
+      {
         int prot = 0;
         if (RESERVED_VA && old_size < new_size) {
             abi_ulong addr;
