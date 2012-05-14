8f5d557c9ba5c32049ae4a28b314e5c13fe04939
Fabien Chouteau <chouteau@adacore.com>
2012-05-14 14:57:33 +0200

    loader: remove elf program overlap check
    This check is bogus when the elf file is a ROM file, memory sizes of
    sections are not relevant.
    
    no-tn-check
    Change-Id: I27c3b6c024ebdb3499ed730b6b73544d77c17620

--- hw/core/loader.c
+++ hw/core/loader.c
@@ -1118,26 +1118,12 @@ static void rom_reset(void *unused)
 
 int rom_check_and_register_reset(void)
 {
-    hwaddr addr = 0;
     MemoryRegionSection section;
     Rom *rom;
-    AddressSpace *as = NULL;
 
     QTAILQ_FOREACH(rom, &roms, next) {
         if (rom->fw_file) {
             continue;
-        }
-        if (!rom->mr) {
-            if ((addr > rom->addr) && (as == rom->as)) {
-                fprintf(stderr, "rom: requested regions overlap "
-                        "(rom %s. free=0x" TARGET_FMT_plx
-                        ", addr=0x" TARGET_FMT_plx ")\n",
-                        rom->name, addr, rom->addr);
-                return -1;
-            }
-            addr  = rom->addr;
-            addr += rom->romsize;
-            as = rom->as;
         }
         section = memory_region_find(rom->mr ? rom->mr : get_system_memory(),
                                      rom->addr, 1);
