3e0caa0b7054913b0841ab3daee9a150c64ced07
Fabien Chouteau <chouteau@adacore.com>
2012-07-03 14:42:21 +0200

    Use clean shutdown request for CTRL-A X
    Part of L329-018.

--- qemu-char.c
+++ qemu-char.c
@@ -352,7 +352,7 @@ static int mux_proc_byte(CharDriverState *chr, MuxDriver *d, int ch)
             {
                  const char *term =  "QEMU: Terminated\n\r";
                  chr->chr_write(chr,(uint8_t *)term,strlen(term));
-                 exit(0);
+                 qemu_system_shutdown_request();
                  break;
             }
         case 's':
