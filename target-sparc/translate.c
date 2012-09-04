df75e2d3b0eab7a5178def283628dceb91d5f903
Tristan Gingold <gingold@chinon.act-europe.fr>
2012-09-04 14:56:36 +0200

    target-sparc/translate.c: stop translation in case of branch.
    Do not put the delay slot instruction in the basic block, as the
    trapping ones modify the branch state.
    
    For L725-033.

--- target-sparc/translate.c
+++ target-sparc/translate.c
@@ -29,6 +29,7 @@
 #include "exec/helper-proto.h"
 #include "tcg-op.h"
 #include "exec/cpu_ldst.h"
+#include "qemu-traces.h"
 
 #include "exec/helper-gen.h"
 
@@ -5342,6 +5343,11 @@ static inline void gen_intermediate_code_internal(SPARCCPU *cpu,
         /* if single step mode, we generate only one instruction and
            generate an exception */
         if (dc->singlestep) {
+            break;
+        }
+        if (tracefile_enabled
+            && (dc->npc == JUMP_PC || dc->npc == DYNAMIC_PC)) {
+
             break;
         }
     } while ((tcg_ctx.gen_opc_ptr < gen_opc_end) &&
