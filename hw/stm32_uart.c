c9d26546cd309be600b54be987aabbf6bf4c65e5
Fabien Chouteau <chouteau@adacore.com>
2013-05-21 12:54:28 +0200

    Add STM32's UART controller
    Part of M314-015.

---
+++ hw/stm32_uart.c
@@ -0,0 +1,233 @@
+/*
+ * QEMU STM32 UART Emulator
+ *
+ * Copyright (c) 2013 AdaCore
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "sysbus.h"
+#include "qemu-char.h"
+
+#include "trace.h"
+
+#define SR_RXNE (1 << 5)
+#define SR_TC   (1 << 6)
+#define SR_TXE  (1 << 7)
+
+#define CR1_TXEIE (1 << 7)
+#define CR1_RXNEIE (1 << 5)
+
+#define FIFO_LENGTH 1024
+
+typedef struct {
+    SysBusDevice busdev;
+    qemu_irq irq;
+    MemoryRegion iomem;
+
+    CharDriverState *chr;
+
+    uint32_t status;
+    uint32_t control;
+
+    /* FIFO */
+    char buffer[FIFO_LENGTH];
+    int  len;
+    int  current;
+
+} stm32_UART_state;
+
+static int uart_data_to_read(stm32_UART_state *uart)
+{
+    return uart->current < uart->len;
+}
+
+static char uart_pop(stm32_UART_state *uart)
+{
+    char ret;
+
+    if (uart->len == 0) {
+        uart->status &= ~SR_RXNE;
+        return 0;
+    }
+
+    ret = uart->buffer[uart->current++];
+
+    if (uart->current >= uart->len) {
+        /* Flush */
+        uart->len     = 0;
+        uart->current = 0;
+    }
+
+    if (!uart_data_to_read(uart)) {
+        uart->status &= ~SR_RXNE;
+    }
+
+    return ret;
+}
+
+static void uart_add_to_fifo(stm32_UART_state *uart,
+                             const uint8_t    *buffer,
+                             int               length)
+{
+    if (uart->len + length > FIFO_LENGTH) {
+        abort();
+    }
+
+    memcpy(uart->buffer + uart->len, buffer, length);
+    uart->len += length;
+}
+
+static int stm32_UART_can_receive(void *opaque)
+{
+    stm32_UART_state *uart = (stm32_UART_state *)opaque;
+
+    return FIFO_LENGTH - uart->len;
+}
+
+static void stm32_UART_receive(void *opaque, const uint8_t *buf, int size)
+{
+    stm32_UART_state *uart = (stm32_UART_state *)opaque;
+
+    uart_add_to_fifo(uart, buf, size);
+    uart->status |= SR_RXNE;
+
+    if (uart->control & CR1_RXNEIE) {
+        qemu_irq_pulse(uart->irq);
+    }
+}
+
+static void stm32_UART_event(void *opaque, int event)
+{
+    /* Nothing */
+}
+
+static uint64_t stm32_UART_read(void *opaque, target_phys_addr_t offset,
+                                   unsigned size)
+{
+    stm32_UART_state *uart = (stm32_UART_state *)opaque;
+
+    switch (offset) {
+    case 0x00: /* Status Register */
+        return uart->status;
+    case 0x04: /* Data Register */
+        return uart_pop(uart);
+    case 0x0C: /* Data Register */
+        return uart->control;
+    default:
+        return 0;
+    }
+}
+
+static void stm32_UART_write(void *opaque, target_phys_addr_t offset,
+                                uint64_t value, unsigned size)
+{
+    stm32_UART_state *uart = (stm32_UART_state *)opaque;
+    unsigned char     c    = 0;
+
+    switch (offset) {
+    case 0x00: /* Status Register */
+        break;
+    case 0x04: /* Data Register */
+        c = value & 0xFF;
+        qemu_chr_fe_write(uart->chr, &c, 1);
+
+        /* Generate interrupt */
+        if (uart->control & CR1_TXEIE) {
+            qemu_irq_pulse(uart->irq);
+        }
+
+        break;
+    case 0x0C: /* Data Register */
+        uart->control = value;
+        break;
+    }
+}
+
+static const MemoryRegionOps stm32_UART_ops = {
+    .read = stm32_UART_read,
+    .write = stm32_UART_write,
+    .endianness = DEVICE_NATIVE_ENDIAN,
+    .valid = {
+        .min_access_size = 2,
+        .max_access_size = 2,
+    },
+};
+
+static int stm32_UART_init(SysBusDevice *dev)
+{
+    stm32_UART_state *uart = FROM_SYSBUS(stm32_UART_state, dev);
+
+    if (uart->chr == NULL) {
+        hw_error("STM32 UART: No chardev attached\n");
+    }
+
+    qemu_chr_add_handlers(uart->chr,
+                          stm32_UART_can_receive,
+                          stm32_UART_receive,
+                          stm32_UART_event,
+                          uart);
+
+    sysbus_init_irq(dev, &uart->irq);
+
+    memory_region_init_io(&uart->iomem, &stm32_UART_ops, uart,
+                          "stm32_UART", 0x400);
+    sysbus_init_mmio(dev, &uart->iomem);
+    return 0;
+}
+
+static void stm32_UART_reset(DeviceState *d)
+{
+    stm32_UART_state *uart = container_of(d, stm32_UART_state, busdev.qdev);
+
+    uart->status =  SR_TC | SR_TXE;
+
+    /* Flush receive FIFO */
+    uart->len = 0;
+    uart->current = 0;
+}
+
+static Property stm32_UART_properties[] = {
+    DEFINE_PROP_CHR("chrdev", stm32_UART_state, chr),
+    DEFINE_PROP_END_OF_LIST(),
+};
+
+static void stm32_UART_class_init(ObjectClass *klass, void *data)
+{
+    DeviceClass *dc = DEVICE_CLASS(klass);
+    SysBusDeviceClass *sdc = SYS_BUS_DEVICE_CLASS(klass);
+
+    sdc->init = stm32_UART_init;
+    dc->reset = stm32_UART_reset;
+    dc->props = stm32_UART_properties;
+}
+
+static TypeInfo stm32_UART_info = {
+    .name          = "stm32_UART",
+    .parent        = TYPE_SYS_BUS_DEVICE,
+    .instance_size = sizeof(stm32_UART_state),
+    .class_init    = stm32_UART_class_init,
+};
+
+static void stm32_UART_register_types(void)
+{
+    type_register_static(&stm32_UART_info);
+}
+
+type_init(stm32_UART_register_types)
