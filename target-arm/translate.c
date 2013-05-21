d47dda562a0d50b28fccbe22160dec7b5f527588
Fabien Chouteau <chouteau@adacore.com>
2013-05-21 12:39:25 +0200

    ARM: Add VFP stacking for M profile
    Part of M314-015.

--- target-arm/translate.c
+++ target-arm/translate.c
@@ -2978,6 +2978,16 @@ static int disas_vfp_insn(CPUARMState * env, DisasContext *s, uint32_t insn)
         return disas_vfp_v8_insn(env, s, insn);
     }
 
+#if 0 /* Lazy VFP stacking not implemented yet */
+    if (arm_feature(env, ARM_FEATURE_M)) {
+        tmp = load_reg(s, 13);
+        /* ???: I don't know how to have an helper without register argument, so
+         * I just pick up one without using it.
+         */
+        gen_helper_v7m_vfp_pre_insn(tmp, cpu_env);
+    }
+#endif
+
     dp = ((insn & 0xf00) == 0xb00);
     switch ((insn >> 24) & 0xf) {
     case 0xe:
@@ -10994,7 +11004,7 @@ static inline void gen_intermediate_code_internal(ARMCPU *cpu,
             break;
         }
 #else
-        if (dc->pc >= 0xfffffff0 && IS_M(env)) {
+        if (dc->pc >= 0xffffff00 && IS_M(env)) {
             /* We always get here via a jump, so know we are not in a
                conditional execution block.  */
             gen_exception_internal(EXCP_EXCEPTION_EXIT);
