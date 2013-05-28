360deae95f0b734ebb0b59666c027849e574359e
Fabien Chouteau <chouteau@adacore.com>
2013-05-28 17:18:17 +0200

    The extended frame flag is in PC not LR
    Part of M314-015.

--- target-arm/helper.c
+++ target-arm/helper.c
@@ -461,7 +461,7 @@ static void do_v7m_exception_exit(CPUARMState *env)
     uint32_t type;
     uint32_t xpsr;
     uint32_t addr;
-    uint32_t lr = env->regs[14];
+    uint32_t pc = env->regs[15];
     int      i;
 
     type = env->regs[15];
@@ -486,7 +486,7 @@ static void do_v7m_exception_exit(CPUARMState *env)
 
     /* Pop VFP */
     if (arm_feature(env, ARM_FEATURE_VFP)) {
-        if (!(lr & 0x10)) {
+        if (!(pc & 0x10)) {
             /* Extended frame */
 
             if ((env->v7m.fpccr & 1) /* LSPACT */) {
