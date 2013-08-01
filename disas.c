94c937c1fea24b854afff8ae7f28706454b33a6c
Fabien Chouteau <chouteau@adacore.com>
2013-08-01 17:44:18 +0200

    Improve Monitor disas with symbol lookup
    To improve the log while hitting rlimit.
    
    Part of M731-018.
    
    Change-Id: I6fa4b4cc777395ea06800fe2bc59fb574c7e1a7c

--- disas.c
+++ disas.c
@@ -642,11 +642,19 @@ void monitor_disas(Monitor *mon, CPUState *cpu,
     }
 
     for(i = 0; i < nb_insn; i++) {
-	monitor_printf(mon, "0x" TARGET_FMT_lx ":  ", pc);
+        const char *sym = lookup_symbol(pc);
+
+        monitor_printf(mon, "0x" TARGET_FMT_lx " ", pc);
+        if (sym[0] != '\0') {
+            monitor_printf(mon, "<%s>:  ", sym);
+        } else {
+            monitor_printf(mon, ":  ");
+        }
         count = s.info.print_insn(pc, &s.info);
-	monitor_printf(mon, "\n");
-	if (count < 0)
-	    break;
+        monitor_printf(mon, "\n");
+        if (count < 0) {
+            break;
+        }
         pc += count;
     }
 }
