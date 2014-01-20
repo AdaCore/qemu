bb2686c4ce16b232843202965ccbe8878a2a67c3
Fabien Chouteau <chouteau@adacore.com>
2014-01-20 18:45:05 +0100

    hw/arm: zynq: fix zynq's UART
    This swaps UART0 and UART1 because UART0 is never used as a character output
    on this board.
    
    TN: M506-013

--- hw/arm/xilinx_zynq.c
+++ hw/arm/xilinx_zynq.c
@@ -296,7 +296,7 @@ static void zynq_init(MachineState *machine)
 
     dev = qdev_new(TYPE_CADENCE_UART);
     busdev = SYS_BUS_DEVICE(dev);
-    qdev_prop_set_chr(dev, "chardev", serial_hd(0));
+    qdev_prop_set_chr(dev, "chardev", serial_hd(1));
     qdev_connect_clock_in(dev, "refclk",
                           qdev_get_clock_out(slcr, "uart0_ref_clk"));
     sysbus_realize_and_unref(busdev, &error_fatal);
@@ -304,7 +304,7 @@ static void zynq_init(MachineState *machine)
     sysbus_connect_irq(busdev, 0, pic[59 - GIC_INTERNAL]);
     dev = qdev_new(TYPE_CADENCE_UART);
     busdev = SYS_BUS_DEVICE(dev);
-    qdev_prop_set_chr(dev, "chardev", serial_hd(1));
+    qdev_prop_set_chr(dev, "chardev", serial_hd(0));
     qdev_connect_clock_in(dev, "refclk",
                           qdev_get_clock_out(slcr, "uart1_ref_clk"));
     sysbus_realize_and_unref(busdev, &error_fatal);
