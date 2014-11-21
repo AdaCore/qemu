27c5f258358a8445e51ee799a72901c015aac55f
Fabien Chouteau <chouteau@adacore.com>
2014-11-21 17:16:11 +0100

    SPARC: Leon and Leon3 ta 0x1 as breakpoint instruction
    Part of NB20-042.
    
    2018-03-16  KONRAD Frederic  <konrad@adacore.com>
            * target/sparc/helper.c: Fix the features access against recent
                                     changes.
    
    Change-Id: I444c302480963b04390ac3a77a83bb95cfa2e4d7

--- target/sparc/cpu.c
+++ target/sparc/cpu.c
@@ -519,7 +519,8 @@ static const sparc_def_t sparc_defs[] = {
         .mmu_sfsr_mask = 0xffffffff,
         .mmu_trcr_mask = 0xffffffff,
         .nwindows = 8,
-        .features = CPU_DEFAULT_FEATURES | CPU_FEATURE_TA0_SHUTDOWN,
+        .features = CPU_DEFAULT_FEATURES | CPU_FEATURE_TA0_SHUTDOWN |
+        CPU_FEATURE_TA1_BREAKPOINT,
     },
     {
         .name = "LEON3",
@@ -534,7 +535,7 @@ static const sparc_def_t sparc_defs[] = {
         .nwindows = 8,
         .features = CPU_DEFAULT_FEATURES | CPU_FEATURE_TA0_SHUTDOWN |
         CPU_FEATURE_ASR17 | CPU_FEATURE_CACHE_CTRL | CPU_FEATURE_POWERDOWN |
-        CPU_FEATURE_CASA,
+        CPU_FEATURE_CASA | CPU_FEATURE_TA1_BREAKPOINT,
     },
 #endif
 };
