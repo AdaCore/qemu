a8544b77eb7d870d2b85d33d158a3227bd68a8fc
Fabien Chouteau <chouteau@adacore.com>
2015-04-30 13:08:31 +0200

    Disable code using FS_IOC_GETFLAGS when it's not defined

--- block/raw-posix.c
+++ block/raw-posix.c
@@ -1651,7 +1651,7 @@ static int raw_create(const char *filename, QemuOpts *opts, Error **errp)
     }
 
     if (nocow) {
-#ifdef __linux__
+#if defined(__linux__)  && defined(FS_IOC_GETFLAGS)
         /* Set NOCOW flag to solve performance issue on fs like btrfs.
          * This is an optimisation. The FS_IOC_SETFLAGS ioctl return value
          * will be ignored since any failure of this operation should not
