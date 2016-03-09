51d017dc7162584366d0b1db91eda9f006d45b78
Fabien Chouteau <chouteau@adacore.com>
2016-03-09 12:12:33 +0100

    Add plug-ins, GNATbus and HostFs to prep

--- hw/ppc/prep.c
+++ hw/ppc/prep.c
@@ -48,6 +48,11 @@
 #include "trace.h"
 #include "elf.h"
 #include "qemu/cutils.h"
+#include "hw/hostfs.h"
+
+#include "qemu-plugin.h"
+#include "gnat-bus.h"
+
 
 /* SMP is not enabled, for now */
 #define MAX_CPUS 1
@@ -667,6 +672,18 @@ static void ppc_prep_init(MachineState *machine)
                          /* XXX: need an option to load a NVRAM image */
                          0,
                          graphic_width, graphic_height, graphic_depth);
+
+
+    /* HostFS */
+    hostfs_create(0xA0000000, sysmem);
+
+    /* Initialize plug-ins */
+    plugin_init(i82378->i8259, 16);
+    plugin_device_init();
+
+    /* Initialize the GnatBus Master */
+    gnatbus_master_init(i82378->i8259, 16);
+    gnatbus_device_init();
 }
 
 static void prep_machine_init(MachineClass *mc)
