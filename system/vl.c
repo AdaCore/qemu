7a5319981284cc5ca3b0eb74e1d18db02491b761
Fabien Chouteau <chouteau@adacore.com>
2016-03-09 12:24:37 +0100

    qemu-traces: add core mechanism
    This commit introduces the core mechanism for the traces generation
    during Qemu execution.
    All architecture specific patches are purposely left in other commits to
    ease maintenance.
    
    Changelog:
    2024-11-04  Clément Chigot  <chigot@adacore.com>
    	* Allow emulation independent traces (gnatemulator#37)
    	* ARM/AARCH64 detects the CPU size.
    
    2023-05-10  Clément Chigot  <chigot@adacore.com>
    	* Rebase for Qemu-8 (W427-004).
    	* Move include under include/adacore.
    	* Move target-related choices inside qemu-traces.c.
    	* Move trace functions in cpu-exec.c to qemu-traces.c.
    	* Separate target patches.
    	* Squashed with:
     	  - qemu-trace: remove TCG jump optimization until traces
    	   (QB08-037)
    	  - qemu-traces: fix last_tb (U730-008)
    2021-06-23  Konrad Frederic  <konrad@adacore.com>
            * qemu-traces.h: add avr (U406-041).
    2021-06-22  Konrad Frederic  <konrad@adacore.com>
            * meson.build: switched from Makefile to meson (U406-041).
    2021-06-02  Frederic Konrad  <konrad@adacore.com>
            * accel/tcg/tcg-all.c: don't allow MTTCG with trace enabled
              (U602-002).
    2020-09-25  Frederic Konrad  <konrad@adacore.com>
            * qemu-traces*: fix the tracing for 64bits target (T924-024).
    2020-06-17  Frederic Konrad  <konrad@adacore.com>
            * qemu-traces.c: only write traces when there are trace to
            write (SB14-048).
    2020-05-19  Frederic Konrad  <konrad@adacore.com>
        * accel/tcg/cpu-exec.c: use uintptr_t instead of unsigned long for
                                trace_after_exec argument (T514-018).
    2020-05-15  Konrad Frederic  <konrad@adacore.com>
            * qemu-traces.c: drop the invalid %m format character (T514-018).
    2021-05-10  Konrad Frederic  <konrad@adacore.com>
            * vl.c: extracted qemu_exit_with_debug (U406-041).
    2020-04-08  KONRAD Frederic  <konrad@adacore.com>
            * qemu-traces.c: check for fwrite return value (T514-016).
    2020-04-08  KONRAD Frederic  <konrad@adacore.com>
            * qemu-options.hx: converted from TEX to REST.
    2019-09-30  KONRAD Frederic  <konrad@adacore.com>
            * qemu-traces.c: add m68k. (S702-051)
    2019-07-22  KONRAD Frederic  <konrad@adacore.com>
            * vl.c: include qapi/qapi-commands-misc.h for
                    qmp_human_monitor_command.
    2019-06-18  KONRAD Frederic  <konrad@adacore.com>
            * vl.c: add qemu_exit_with_debug.
    2019-02-01  KONRAD Frederic  <konrad@adacore.com>
            * tcg/tcg-op.c: catch the unhandled gen_lookup_and_goto_ptr when
              traces are enabled.
    2018-12-20  KONRAD Frederic  <konrad@adacore.com>
            * qemu-traces.c: fix the exit_handler deadlock. (RB13-022)
    2018-12-06  KONRAD Frederic  <konrad@adacore.com>
            * qemu-traces.c: add exec-trace-limit option. (RB12-022)
    2018-07-17  KONRAD Frederic  <konrad@adacore.com>
            * target/i386/translate.c: fix against the new translation_loop.
            * target/ppc/translate.c: fix against the new changes
    2018-07-18  KONRAD Frederic  <konrad@adacore.com>
            * changes extracted from "New machine: wrSbc8641d":
            * renamed trace_init to exec_trace_init due to conflict with module.
            * ditto for trace_push_entry, trace_cleanup.
            * introduced exec_trace_special.
    2018-07-26  KONRAD Frederic  <konrad@adacore.com>
            * tcg/tcg-op.c: don't assert in case of a TCG_EXIT_NOPATCH label.
    2018-08-30  KONRAD Frederic  <konrad@adacore.com>
            * qemu-traces.c: added ELF_MACHINE for riscv.
    
    TN: I108-020, NA24-010, N506-003, L614-031, U730-008
    
    qemu-traces: split histmap header verification
    
    Introduce a more verbose helper to check the value within the headers.
    
    eng/shared/release#1025
    gnatemulator#37

--- system/vl.c
+++ system/vl.c
@@ -135,6 +135,8 @@
 #include "qemu/guest-random.h"
 #include "qemu/keyval.h"
 
+#include "adacore/qemu-traces.h"
+
 #define MAX_VIRTIO_CONSOLES 1
 
 #include "hw/adacore/rlimit.h"
@@ -3712,6 +3714,12 @@ void qemu_init(int argc, char **argv)
                 }
                 quick_monitor_cmd = optarg;
                 break;
+            case QEMU_OPTION_exec_trace:
+                exec_trace_opts_parse(optarg);
+                break;
+            case QEMU_OPTION_exec_trace_limit:
+                exec_trace_limit(optarg);
+                break;
 #if defined(CONFIG_POSIX)
             case QEMU_OPTION_runas:
                 warn_report("-runas is deprecated, use '-run-with user=...' instead");
@@ -3887,6 +3895,8 @@ void qemu_init(int argc, char **argv)
     if (!preconfig_requested) {
         qmp_x_exit_preconfig(&error_fatal);
     }
+
+    exec_trace_init();
     qemu_init_displays();
     accel_setup_post(current_machine);
     os_setup_post();
