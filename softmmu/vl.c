d26a60a25013c28d0fa533e6244c1c5f566c65c2
Fabien Chouteau <chouteau@adacore.com>
2016-03-09 12:26:47 +0100

    hw/adacore: add new rlimit feature in QEMU
    2021-05-10  KONRAD Frederic  <konrad@adacore.com>
            * vl.c: move the code to hw/adacore/rlimit.c to avoid future
            conflicts.  (U406-041)
    2020-04-08  KONRAD Frederic  <konrad@adacore.com>
            * qemu-options.hx: converted from TEX to REST.
    2019-06-18  KONRAD Frederic  <konrad@adacore.com>
            * vl.c: extract out qemu_exit_with_debug()
    2018-12-10  KONRAD Frederic  <konrad@adacore.com>
            * vl.c: print the PC in hexadecimal format. (RA25-011)
    
    TN: M731-018

--- softmmu/vl.c
+++ softmmu/vl.c
@@ -137,6 +137,7 @@
 
 #define MAX_VIRTIO_CONSOLES 1
 
+#include "hw/adacore/rlimit.h"
 void qemu_exit_with_debug(const char *fmt, ...);
 
 typedef struct BlockdevOptionsQueueEntry {
@@ -1891,6 +1892,8 @@ static void qemu_create_early_backends(void)
 
     object_option_foreach_add(object_create_early);
 
+    rlimit_init();
+
     /* spice needs the timers to be initialized by this point */
     /* spice must initialize before audio as it changes the default audiodev */
     /* spice must initialize before chardevs (for spicevmc and spiceport) */
@@ -3531,6 +3534,9 @@ void qemu_init(int argc, char **argv)
                 break;
             case QEMU_OPTION_nouserconfig:
                 /* Nothing to be parsed here. Especially, do not error out below. */
+                break;
+            case QEMU_OPTION_rlimit:
+                rlimit_set_value(optarg);
                 break;
             default:
                 if (os_parse_cmd_args(popt->index, optarg)) {
