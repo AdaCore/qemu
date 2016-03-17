a0aeb932d35a6c5d0aad2b5b06597747b2588c15
Fabien Chouteau <chouteau@adacore.com>
2016-03-17 18:53:29 +0100

    Disable warning message in block
    no-tn-check
    
    Change-Id: I368d903ab9bdd232f75c9462b537318ed28e6049

--- block/raw-format.c
+++ block/raw-format.c
@@ -437,18 +437,6 @@ static int raw_open(BlockDriverState *bs, QDict *options, int flags,
         ((BDRV_REQ_FUA | BDRV_REQ_MAY_UNMAP | BDRV_REQ_NO_FALLBACK) &
             bs->file->bs->supported_zero_flags);
 
-    if (bs->probed && !bdrv_is_read_only(bs)) {
-        bdrv_refresh_filename(bs->file->bs);
-        fprintf(stderr,
-                "WARNING: Image format was not specified for '%s' and probing "
-                "guessed raw.\n"
-                "         Automatically detecting the format is dangerous for "
-                "raw images, write operations on block 0 will be restricted.\n"
-                "         Specify the 'raw' format explicitly to remove the "
-                "restrictions.\n",
-                bs->file->bs->filename);
-    }
-
     ret = raw_read_options(options, bs, s, errp);
     if (ret < 0) {
         return ret;
