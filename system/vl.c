5d94a42fdf3b8393d270a6c601a9edcb2df073c8
Fabien Chouteau <chouteau@adacore.com>
2016-04-12 20:00:45 +0200

    New option: -monitor-cmd give the output of a monitor command
    2023-03-27  CHIGOT Clément  <chigot@adacore.com>
    	* Fix exit code (W316-011)
    2020-04-15  KONRAD Frederic  <konrad@adacore.com>
            * softmmu/vl.c: use exit instead of return (SB14-048).
    2020-04-08  KONRAD Frederic  <konrad@adacore.com>
            * qemu-options.hx: converted from TEX to REST.
    
    TN: NC03-025, W316-001

--- system/vl.c
+++ system/vl.c
@@ -168,6 +168,7 @@ typedef struct DeviceOption {
     QTAILQ_ENTRY(DeviceOption) next;
 } DeviceOption;
 
+static const char *quick_monitor_cmd;
 static const char *cpu_option;
 static const char *mem_path;
 static const char *incoming;
@@ -3796,6 +3797,14 @@ void qemu_init(int argc, char **argv)
             case QEMU_OPTION_rlimit:
                 rlimit_set_value(optarg);
                 break;
+            case QEMU_OPTION_monitor_cmd:
+                if (quick_monitor_cmd) {
+                    fprintf(stderr,
+                            "qemu: only one monitor-cmd option may be given\n");
+                    exit(1);
+                }
+                quick_monitor_cmd = optarg;
+                break;
 #if defined(CONFIG_POSIX) && !defined(EMSCRIPTEN)
             case QEMU_OPTION_daemonize:
                 os_set_daemonize(true);
@@ -3982,4 +3991,12 @@ void qemu_init(int argc, char **argv)
         os_setup_post();
     }
     resume_mux_open();
+
+    if (quick_monitor_cmd != NULL) {
+        printf("%s", qmp_human_monitor_command(quick_monitor_cmd,
+                                               false /* has_cpu_index */,
+                                               0 /* cpu_index      */,
+                                               NULL));
+        exit(0);
+    }
 }
