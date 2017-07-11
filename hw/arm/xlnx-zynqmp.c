1312387f2e11ce67747f2feb691027fd102451a8
KONRAD Frederic <konrad@adacore.com>
2017-07-11 15:10:01 +0200

    hw/arm: zynqmp: add gnatbus
    This adds gnatbus to the zynqmp.
    
    TN: Q323-013

--- hw/arm/xlnx-zynqmp.c
+++ hw/arm/xlnx-zynqmp.c
@@ -25,6 +25,8 @@
 #include "sysemu/kvm.h"
 #include "sysemu/sysemu.h"
 #include "kvm_arm.h"
+#include "sysemu/sysemu.h"
+#include "hw/adacore/gnat-bus.h"
 
 #define GIC_NUM_SPI_INTR 160
 
@@ -452,7 +454,7 @@ static void xlnx_zynqmp_realize(DeviceState *dev, Error **errp)
     int num_apus = MIN(ms->smp.cpus, XLNX_ZYNQMP_NUM_APU_CPUS);
     const char *boot_cpu = s->boot_cpu ? s->boot_cpu : "apu-cpu[0]";
     ram_addr_t ddr_low_size, ddr_high_size;
-    qemu_irq gic_spi[GIC_NUM_SPI_INTR];
+    qemu_irq *gic_spi = g_new(qemu_irq, GIC_NUM_SPI_INTR);
     Error *err = NULL;
 
     ram_size = memory_region_size(s->ddr_ram);
@@ -850,6 +852,10 @@ static void xlnx_zynqmp_realize(DeviceState *dev, Error **errp)
         sysbus_connect_irq(SYS_BUS_DEVICE(&s->usb[i].sysbus_xhci), 3,
                            gic_spi[usb_intr[i] + 3]);
     }
+
+    /* Initialize the GnatBus Master */
+    gnatbus_master_init(gic_spi, 128);
+    gnatbus_device_init();
 }
 
 static Property xlnx_zynqmp_props[] = {
