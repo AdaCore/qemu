dc7d4fb94d1a3895afda8ce10be744784798b6d1
KONRAD Frederic <konrad@adacore.com>
2017-09-20 12:53:22 +0200

    i386/pc: add hostfs and parameters
    This adds the hostfs and parameters based @0xf3000000.
    This means qemu-system-i386 will have it (either x86-linux or x86-vx7).
    
    TN: Q728-025

--- hw/i386/pc_piix.c
+++ hw/i386/pc_piix.c
@@ -66,6 +66,12 @@
 #include "hw/i386/acpi-build.h"
 #include "target/i386/cpu.h"
 
+#include "hw/adacore/hostfs.h"
+
+#define HOSTFS_START (0xf3082000)
+#define PARAMS_ADDR  (0xf3080000)
+#define PARAMS_SIZE  (0x01000)
+
 #define XEN_IOAPIC_NUM_PIRQS 128ULL
 
 static GlobalProperty pc_piix_compat_defaults[] = {
@@ -113,6 +119,8 @@ static void pc_init1(MachineState *machine, const char *pci_type)
     GSIState *gsi_state;
     MemoryRegion *ram_memory;
     MemoryRegion *pci_memory = NULL;
+    MemoryRegion *params;
+    const char *kernel_cmdline;
     ram_addr_t lowmem;
     uint64_t hole64_size = 0;
     PCIDevice *pci_dev;
@@ -120,6 +128,18 @@ static void pc_init1(MachineState *machine, const char *pci_type)
     size_t i;
 
     assert(pcmc->pci_enabled);
+
+    /* Here are the HostFS and parameters */
+    params = g_new(MemoryRegion, 1);
+    memory_region_init_ram(params, NULL, "params", PARAMS_SIZE,
+                           &error_fatal);
+    memory_region_add_subregion(get_system_memory(), PARAMS_ADDR, params);
+    /* HostFS */
+    hostfs_create(HOSTFS_START, get_system_memory());
+    kernel_cmdline = machine->kernel_cmdline ? machine->kernel_cmdline : "";
+    cpu_physical_memory_write(PARAMS_ADDR, kernel_cmdline,
+                              strlen(kernel_cmdline) + 1);
+    memory_region_set_readonly(params, true);
 
     /*
      * Calculate ram split, for memory below and above 4G.  It's a bit
