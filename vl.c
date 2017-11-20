9ea3a335d5478653d86586793ae8e15286ef6e35
Frederic KONRAD <konrad@adacore.com>
2017-11-20 15:15:43 +0100

    win32: exit deadlock workaround
    There is a deadlock happening rarely in KERNEL32!ExitProcess.
    
    According to MSDN:
    If one of the terminated threads in the process holds a lock and the
    DLL detach code in one of the loaded DLLs attempts to acquire the same
    lock, then calling ExitProcess results in a deadlock. In contrast, if
    a process terminates by calling TerminateProcess, the DLLs that the
    process is attached to are not notified of the process termination.
    
    So this patch works around the deadlock by calling TerminateProcess
    instead of simply exiting the main(..) function.
    
    TN: QA10-046
    no-precommit-check
    Change-Id: Ia4e641cf0f1a4daf29994a2dacc92f9d68388625

--- vl.c
+++ vl.c
@@ -4859,5 +4859,16 @@ int main(int argc, char **argv, char **envp)
     user_creatable_cleanup();
     /* TODO: unref root container, check all devices are ok */
 
+    /* Threads are not exited correctly on Windows.
+     * Since we didn't find the bug yet lets kill the process at the end
+     * to avoid deadlock in Windows DLLs.
+     */
+#if defined(_WIN32)
+    exec_trace_cleanup();
+    DWORD pid = GetCurrentProcessId();
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS,
+                                  false, pid);
+    TerminateProcess(hProcess, 0);
+#endif /* WIN32 */
     return 0;
 }
