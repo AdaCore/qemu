1d9befcb99b2dce65d1bc9ee39649c4f7ea4b021
Fabien Chouteau <chouteau@adacore.com>
2018-07-19 11:47:51 +0200

    hw/adacore: add GNATBus
    TN: K125-030

--- system/vl.c
+++ system/vl.c
@@ -135,6 +135,7 @@
 #include "qemu/keyval.h"
 
 #include "adacore/qemu-traces.h"
+#include "hw/adacore/gnat-bus.h"
 
 #define MAX_VIRTIO_CONSOLES 1
 
@@ -3716,6 +3717,12 @@ void qemu_init(int argc, char **argv)
                 break;
             case QEMU_OPTION_exec_trace_limit:
                 exec_trace_limit(optarg);
+                break;
+            case QEMU_OPTION_gnatbus:
+                gnatbus_save_optargs(optarg);
+                break;
+            case QEMU_OPTION_gnatbus_timeout:
+                gnatbus_save_timeout_optargs(optarg);
                 break;
 #if defined(CONFIG_POSIX)
             case QEMU_OPTION_runas:
