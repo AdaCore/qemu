3c85a579b14bb3e95c8f71d72493e761385ce41d
Fabien Chouteau <chouteau@adacore.com>
2020-03-17 15:03:29 +0100

    hw/arm: zynq: Add init HostFS and GNATbus
    TN: SC12-020

--- hw/arm/xilinx_zynq.c
+++ hw/arm/xilinx_zynq.c
@@ -47,6 +47,9 @@ OBJECT_DECLARE_SIMPLE_TYPE(ZynqMachineState, ZYNQ_MACHINE)
 /* board base frequency: 33.333333 MHz */
 #define PS_CLK_FREQUENCY (100 * 1000 * 1000 / 3)
 
+#include "hw/adacore/gnat-bus.h"
+#include "hw/adacore/hostfs.h"
+
 #define NUM_SPI_FLASHES 4
 #define NUM_QSPI_FLASHES 2
 #define NUM_QSPI_BUSSES 2
@@ -208,6 +211,7 @@ static void zynq_init(MachineState *machine)
     DeviceState *dev, *slcr;
     SysBusDevice *busdev;
     qemu_irq pic[64];
+    qemu_irq *gnatbus_irqs;
     int n;
     unsigned int smp_cpus = machine->smp.cpus;
 
@@ -281,8 +285,12 @@ static void zynq_init(MachineState *machine)
                            qdev_get_gpio_in(cpudev, ARM_CPU_FIQ));
     }
 
+    /* gnatbus IRQs */
+    gnatbus_irqs = qemu_allocate_irqs(NULL, NULL, 64);
+
     for (n = 0; n < 64; n++) {
         pic[n] = qdev_get_gpio_in(dev, n);
+        gnatbus_irqs[n] = qdev_get_gpio_in(dev, n);
     }
 
     n = zynq_init_spi_flashes(0xE0006000, pic[58 - IRQ_OFFSET], false, 0);
@@ -446,6 +454,13 @@ static void zynq_init(MachineState *machine)
     create_unimplemented_device("zynq.qos301_cpu", 0xF8946000, 0x130);
     create_unimplemented_device("zynq.qos301_dmac", 0xF8947000, 0x130);
     create_unimplemented_device("zynq.qos301_iou", 0xF8948000, 0x130);
+
+    /* Initialize the GnatBus Master */
+    gnatbus_master_init(gnatbus_irqs, 64);
+    gnatbus_device_init();
+
+    /* HostFS */
+    hostfs_create(0xF800F000, get_system_memory());
 
     zynq_binfo.ram_size = machine->ram_size;
     zynq_binfo.board_id = 0xd32;
