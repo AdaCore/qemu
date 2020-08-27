5fe86e8c96ec477e210bc9742538d0edf0f0f097
KONRAD Frederic <konrad@adacore.com>
2020-08-27 14:11:04 +0200

    gdbstub: i386: send ST(X) instead of FPR(X)
    This fixes a bug with the x87 registers and GDB.  GDB expects the STx
    registers to be sent, but unfortunately QEMU is sending the FPRx registers.
    
    This breaks inferior calls in GDB since it expects to find the result in the
    ST0 registers.
    
    TN: T826-038
    Change-Id: I970e1f6dc117d38b621fb399647abc57e012d9dc

--- target/i386/gdbstub.c
+++ target/i386/gdbstub.c
@@ -78,6 +78,8 @@ static const int gpr_map32[8] = { 0, 1, 2, 3, 4, 5, 6, 7 };
 #define GDB_FORCE_64 0
 #endif
 
+#define ST(n)  (env->fpregs[(env->fpstt + (n)) & 7].d)
+
 
 int x86_cpu_gdb_read_register(CPUState *cs, GByteArray *mem_buf, int n)
 {
@@ -104,7 +106,7 @@ int x86_cpu_gdb_read_register(CPUState *cs, GByteArray *mem_buf, int n)
             return gdb_get_reg32(mem_buf, env->regs[gpr_map32[n]]);
         }
     } else if (n >= IDX_FP_REGS && n < IDX_FP_REGS + 8) {
-        floatx80 *fp = (floatx80 *) &env->fpregs[n - IDX_FP_REGS];
+        floatx80 *fp = (floatx80 *) &ST(n - IDX_FP_REGS);
         int len = gdb_get_reg64(mem_buf, cpu_to_le64(fp->low));
         len += gdb_get_reg16(mem_buf, cpu_to_le16(fp->high));
         return len;
@@ -290,7 +292,7 @@ int x86_cpu_gdb_write_register(CPUState *cs, uint8_t *mem_buf, int n)
             return 4;
         }
     } else if (n >= IDX_FP_REGS && n < IDX_FP_REGS + 8) {
-        floatx80 *fp = (floatx80 *) &env->fpregs[n - IDX_FP_REGS];
+        floatx80 *fp = (floatx80 *) &ST(n - IDX_FP_REGS);
         fp->low = le64_to_cpu(* (uint64_t *) mem_buf);
         fp->high = le16_to_cpu(* (uint16_t *) (mem_buf + 8));
         return 10;
