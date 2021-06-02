2e8e0ea56cde772568557333962f83676b732e52
Frederic Konrad <konrad@adacore.com>
2021-06-02 12:39:26 +0200

    xlnx-zynqmp: fix the RPU cpu count
    This fixes num_rpus for -smp < 4.
    
    TN: U602-002
    Change-Id: I0fba55522cf851ecd065b067648402a49a80efa5

--- hw/arm/xlnx-zynqmp.c
+++ hw/arm/xlnx-zynqmp.c
@@ -92,6 +92,10 @@
 
 #define SDHCI_CAPABILITIES  0x280737ec6481 /* Datasheet: UG1085 (v1.7) */
 
+/* No need to check for the RPU maximum, since smp is limited to 6.  */
+#define NUM_RPUS (ms->smp.cpus < XLNX_ZYNQMP_NUM_APU_CPUS ?     \
+                  0 : ms->smp.cpus - XLNX_ZYNQMP_NUM_APU_CPUS)
+
 static const uint64_t gem_addr[XLNX_ZYNQMP_NUM_GEMS] = {
     0xFF0B0000, 0xFF0C0000, 0xFF0D0000, 0xFF0E0000,
 };
@@ -279,10 +283,8 @@ static void xlnx_zynqmp_create_rpu(MachineState *ms, XlnxZynqMPState *s,
                                    const char *boot_cpu, Error **errp)
 {
     int i;
-    int num_rpus = MIN(ms->smp.cpus - XLNX_ZYNQMP_NUM_APU_CPUS,
-                       XLNX_ZYNQMP_NUM_RPU_CPUS);
 
-    if (num_rpus <= 0) {
+    if (NUM_RPUS <= 0) {
         /* Don't create rpu-cluster object if there's nothing to put in it */
         return;
     }
@@ -291,7 +293,7 @@ static void xlnx_zynqmp_create_rpu(MachineState *ms, XlnxZynqMPState *s,
                             TYPE_CPU_CLUSTER);
     qdev_prop_set_uint32(DEVICE(&s->rpu_cluster), "cluster-id", 1);
 
-    for (i = 0; i < num_rpus; i++) {
+    for (i = 0; i < NUM_RPUS; i++) {
         const char *name;
 
         object_initialize_child(OBJECT(&s->rpu_cluster), "rpu-cpu[*]",
@@ -425,8 +427,6 @@ static void xlnx_zynqmp_init(Object *obj)
     XlnxZynqMPState *s = XLNX_ZYNQMP(obj);
     int i;
     int num_apus = MIN(ms->smp.cpus, XLNX_ZYNQMP_NUM_APU_CPUS);
-    int num_rpus = MIN(ms->smp.cpus - XLNX_ZYNQMP_NUM_APU_CPUS,
-                       XLNX_ZYNQMP_NUM_RPU_CPUS);
 
     object_initialize_child(obj, "apu-cluster", &s->apu_cluster,
                             TYPE_CPU_CLUSTER);
@@ -440,7 +440,7 @@ static void xlnx_zynqmp_init(Object *obj)
 
     object_initialize_child(obj, "gic", &s->gic, gic_class_name());
 
-    if (num_rpus > 0) {
+    if (NUM_RPUS > 0) {
         /* Do not create the rpu_gic in case we don't have rpus..  */
         object_initialize_child(obj, "rpu_gic", &s->rpu_gic,
                                 gic_class_name());
@@ -509,8 +509,6 @@ static void xlnx_zynqmp_realize(DeviceState *dev, Error **errp)
     uint8_t i;
     uint64_t ram_size;
     int num_apus = MIN(ms->smp.cpus, XLNX_ZYNQMP_NUM_APU_CPUS);
-    int num_rpus = MIN(ms->smp.cpus - XLNX_ZYNQMP_NUM_APU_CPUS,
-                       XLNX_ZYNQMP_NUM_RPU_CPUS);
 
     const char *boot_cpu = s->boot_cpu ? s->boot_cpu : "apu-cpu[0]";
     ram_addr_t ddr_low_size, ddr_high_size;
@@ -569,11 +567,11 @@ static void xlnx_zynqmp_realize(DeviceState *dev, Error **errp)
     qdev_prop_set_bit(DEVICE(&s->gic),
                       "has-virtualization-extensions", s->virt);
 
-    if (num_rpus > 0) {
+    if (NUM_RPUS > 0) {
         qdev_prop_set_uint32(DEVICE(&s->rpu_gic), "num-irq",
                              GIC_NUM_SPI_INTR + 32);
         qdev_prop_set_uint32(DEVICE(&s->rpu_gic), "revision", 1);
-        qdev_prop_set_uint32(DEVICE(&s->rpu_gic), "num-cpu", num_rpus);
+        qdev_prop_set_uint32(DEVICE(&s->rpu_gic), "num-cpu", NUM_RPUS);
         qdev_prop_set_uint32(DEVICE(&s->rpu_gic), "first-cpu-index", 4);
     }
 
@@ -685,12 +683,12 @@ static void xlnx_zynqmp_realize(DeviceState *dev, Error **errp)
         return;
     }
 
-    if (num_rpus > 0) {
+    if (NUM_RPUS > 0) {
         if (!sysbus_realize(SYS_BUS_DEVICE(&s->rpu_gic), errp)) {
             return;
         }
 
-        for (i = 0; i < num_rpus; i++) {
+        for (i = 0; i < NUM_RPUS; i++) {
             qemu_irq irq;
 
             sysbus_mmio_map(SYS_BUS_DEVICE(&s->rpu_gic), i + 1,
@@ -698,13 +696,13 @@ static void xlnx_zynqmp_realize(DeviceState *dev, Error **errp)
             sysbus_connect_irq(SYS_BUS_DEVICE(&s->rpu_gic), i,
                                qdev_get_gpio_in(DEVICE(&s->rpu_cpu[i]),
                                                 ARM_CPU_IRQ));
-            sysbus_connect_irq(SYS_BUS_DEVICE(&s->rpu_gic), i + num_rpus,
+            sysbus_connect_irq(SYS_BUS_DEVICE(&s->rpu_gic), i + NUM_RPUS,
                                qdev_get_gpio_in(DEVICE(&s->rpu_cpu[i]),
                                                 ARM_CPU_FIQ));
-            sysbus_connect_irq(SYS_BUS_DEVICE(&s->rpu_gic), i + num_rpus * 2,
+            sysbus_connect_irq(SYS_BUS_DEVICE(&s->rpu_gic), i + NUM_RPUS * 2,
                                qdev_get_gpio_in(DEVICE(&s->rpu_cpu[i]),
                                                 ARM_CPU_VIRQ));
-            sysbus_connect_irq(SYS_BUS_DEVICE(&s->rpu_gic), i + num_rpus * 3,
+            sysbus_connect_irq(SYS_BUS_DEVICE(&s->rpu_gic), i + NUM_RPUS * 3,
                                qdev_get_gpio_in(DEVICE(&s->rpu_cpu[i]),
                                                 ARM_CPU_VFIQ));
             irq = qdev_get_gpio_in(DEVICE(&s->rpu_gic),
@@ -730,7 +728,7 @@ static void xlnx_zynqmp_realize(DeviceState *dev, Error **errp)
     }
 
     for (i = 0; i < GIC_NUM_SPI_INTR; i++) {
-        if (num_rpus > 0) {
+        if (NUM_RPUS > 0) {
             gic_spi[i] = qemu_irq_split(
                 qdev_get_gpio_in(DEVICE(&s->gic), i),
                 qdev_get_gpio_in(DEVICE(&s->rpu_gic), i));
