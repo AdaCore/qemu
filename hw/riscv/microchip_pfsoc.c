ed64f2fa5952a723c4120091d8c3d3908c23d289
Frederic Konrad <konrad@adacore.com>
2021-07-15 15:41:24 +0200

    riscv64: microchip_pfsoc: add hostfs
    This adds the hostfs to the Microchip Polarfire BSP.
    
    TN: U716-022
    Change-Id: I70cf93c6cb3112c0406f36faa7bdc267ee1164e6

--- hw/riscv/microchip_pfsoc.c
+++ hw/riscv/microchip_pfsoc.c
@@ -55,6 +55,7 @@
 #include "hw/intc/sifive_plic.h"
 #include "sysemu/sysemu.h"
 #include "hw/adacore/gnat-bus.h"
+#include "hw/adacore/hostfs.h"
 
 /*
  * The BIOS image used by this machine is called Hart Software Services (HSS).
@@ -90,6 +91,7 @@
 static const MemMapEntry microchip_pfsoc_memmap[] = {
     [MICROCHIP_PFSOC_RSVD0] =           {        0x0,      0x100 },
     [MICROCHIP_PFSOC_DEBUG] =           {      0x100,      0xf00 },
+    [MICROCHIP_PFSOC_HOSTFS] =          {   0x101000,     0x1000 },
     [MICROCHIP_PFSOC_E51_DTIM] =        {  0x1000000,     0x2000 },
     [MICROCHIP_PFSOC_BUSERR_UNIT0] =    {  0x1700000,     0x1000 },
     [MICROCHIP_PFSOC_BUSERR_UNIT1] =    {  0x1701000,     0x1000 },
@@ -436,6 +438,9 @@ static void microchip_pfsoc_soc_realize(DeviceState *dev, Error **errp)
     }
     gnatbus_master_init(cpu_irqs, MICROCHIP_PFSOC_PLIC_NUM_SOURCES);
     gnatbus_device_init();
+
+    /* HostFS */
+    hostfs_create(memmap[MICROCHIP_PFSOC_HOSTFS].base, get_system_memory());
 }
 
 static void microchip_pfsoc_soc_class_init(ObjectClass *oc, void *data)
